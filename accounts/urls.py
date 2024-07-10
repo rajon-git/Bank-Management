9dc6922aa56e18d5f37533e47cc95f48c56056a2
rajon-git <rajon.zhsust15@gmail.com>
2024-07-10 22:58:01 +0600

    profile updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("register/", views.UserRegistrationView.as_view(), name="register"),
     path("login/", views.UserLoginView.as_view(), name="login"),
-    path("logout/", views.UserLoginView.as_view(), name="logout"),
+    path("logout/", views.logout_user, name="logout"),
+    path("profile/", views.UserBankAccountUpdateView.as_view(), name="profile"),
 ]
