cf5585b99f853c48f444e0912a7e7b7369bb71ef
rajon-git <rajon.zhsust15@gmail.com>
2024-07-10 21:46:10 +0600

    login and logout views and also url create

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("register/", views.UserRegistrationView.as_view(), name="register"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", views.UserLoginView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import FormView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserRegistrationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -14,5 +15,16 @@ class UserRegistrationView(FormView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        print(user)
         return super().form_valid(form)
+
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
