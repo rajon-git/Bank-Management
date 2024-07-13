b916b61b63cca17f25d2c073e806f9975d4365fe
rajon-git <rajon.zhsust15@gmail.com>
2024-07-13 12:51:46 +0600

    withdraw and loan request view create

--- transactions/views.py
+++ transactions/views.py
@@ -6,6 +6,7 @@ from .models import Transaction
 from .forms import DepositeForm, WithDrawForm, LoanRequestForm
 from .constants import DEPOSIT, WITHDRAWL, LOAN, LOAN_PAID
 from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -44,4 +45,40 @@ class depositeMoneyView(TransactionCreateMixin):
             update_fields = ['balance']
         )
         messages.success(self.request, f"{amount} BDT was deposited to your account succeffully")
+        return super().form_valid(form)
+    
+class WithdrawMoneyView(TransactionCreateMixin):
+    form_class = WithDrawForm
+    title = 'Withdraw Money'
+
+    def get_initial(self):
+        initial = {'transaction_type': WITHDRAWL}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields = ['balance']
+        )
+        messages.success(self.request, f"Successfully withdraw from your account {amount} BDT")
+        return super().form_valid(form)
+    
+
+class LoanRequestView(TransactionCreateMixin):
+    form_class = LoanRequestForm
+    title = 'Request for Loan'
+
+    def get_initial(self):
+        initial = {'transaction_type': LOAN}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        current_loan_count = Transaction.objects.filter(account = self.request.user.account , transaction_type = 3, loan_approve=True).count()
+
+        if current_loan_count > 3:
+            return HttpResponse("You have cross your limit")
+        messages.success(self.request, f"Loan request for {amount} BDT have been successfully send to the admin")
         return super().form_valid(form)
