cac0b90cafb2b7be15dd5f00d3242a9ba00ebab3
rajon-git <rajon.zhsust15@gmail.com>
2024-07-13 14:03:21 +0600

    transaction report view create

--- transactions/views.py
+++ transactions/views.py
@@ -1,12 +1,15 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Transaction
 from .forms import DepositeForm, WithDrawForm, LoanRequestForm
 from .constants import DEPOSIT, WITHDRAWL, LOAN, LOAN_PAID
 from django.contrib import messages
 from django.http import HttpResponse
+from datetime import datetime
+from django.db import Sum
 
 # Create your views here.
 
@@ -82,3 +85,35 @@ class LoanRequestView(TransactionCreateMixin):
             return HttpResponse("You have cross your limit")
         messages.success(self.request, f"Loan request for {amount} BDT have been successfully send to the admin")
         return super().form_valid(form)
+    
+class TransactionReportView(LoginRequiredMixin, ListView):
+    template_name = ""
+    model = Transaction 
+    balance = 0
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(
+            account= self.request.user.account
+        )
+        start_date_str = self.request.GET.get('start_date')
+        end_date_str = self.request.GET.get('end_date')
+
+        if start_date_str and end_date_str:
+            start_date = datetime.strptime(start_date_str, "%y-%m-%d").date()
+            end_date = datetime.strptime(end_date_str, "%y-%m-%d").date()
+
+            # queryset = queryset.filter(timestamp_date_gte = start_date, timestamp_date_lte = end_date)
+
+            self.balance = Transaction.objects.filter(timestamp_date_gte = start_date, timestamp_date_lte = end_date).aggregate(Sum('amount'))['amount_sum']
+
+        else:
+            self.balance = self.request.user.account.balance
+
+        return queryset.distinct()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'account' : self.request.user.account
+        })
+        return context
